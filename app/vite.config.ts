f767acdea86d5b97657ad462086468619019692f
Amogh Biju <amogh@outlook.com>
2026-02-22 03:34:23 -0800

    Update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/supreme-octo-funicular/',
+  base: '/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
